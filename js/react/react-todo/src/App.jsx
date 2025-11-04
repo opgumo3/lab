cb1ea54e9a35668ee30cf8046dcf77c25275b2d6
Gumo <opgumo@naver.com>
2025-11-04 21:48:44 +0900

    react: todo component 분리

--- js/react/react-todo/src/App.jsx
+++ js/react/react-todo/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Lists from './components/Lists'
+import Form from './components/Form'
 
 function App() {
+
+  const [value, setValue] = useState('')
 
   const [todoData, setTodoData] = useState([
     {
@@ -16,49 +19,27 @@ function App() {
       completed: false,
     }
   ])
-  const [value, setValue] = useState('')
-
-  const handleChange = (e) => {
-    setValue(e.target.value)
-  }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+        e.preventDefault()
 
-    let newTodo = {
-      id: Date.now(),
-      title: value,
-      completed: false,
+        let newTodo = {
+            id: Date.now(),
+            title: value,
+            completed: false,
+        }
+
+        setTodoData([...todoData, newTodo]);
+        setValue('')
     }
-
-    setTodoData([...todoData, newTodo]);
-    setValue('')
-  }
 
   return (
     <>
       <div className='container'>
         <div className='todoBlock'>
           <h1 className='title'>TodoList</h1>
-
-          <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-            <input
-              type='text'
-              name='value'
-              style={{ flex: '10', padding: '5px' }}
-              placeholder='할 일을 입력하세요'
-              value={value}
-              onChange={(e) => handleChange(e)}
-            />
-            <input
-              type='submit'
-              value='입력'
-              className='btn'
-              style={{ flex: '1' }}
-            />
-          </form>
-
           <Lists todoData={todoData} setTodoData={setTodoData} />
+          <Form value={value} handleSubmit={handleSubmit} setValue={setValue}/>
         </div>
       </div>
     </>
