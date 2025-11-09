50c284ad85fefe1fe39bf55ee915adc5cc3af16b
Gumo <opgumo@naver.com>
2025-11-09 14:12:15 +0900

    solving settig

---
+++ java/solving/app/src/main/java/solving/programmers/lv2/TargetNumber.java
@@ -0,0 +1,12 @@
+package solving.programmers.lv2;
+
+// 타겟 넘버
+public class TargetNumber {
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+
+    
+}
